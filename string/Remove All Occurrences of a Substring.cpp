e6f9de6b14f63cde921ac37b5426e0655ab998e6
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-02-26 21:04:34 +0530

    add new file

--- string/Remove All Occurrences of a Substring.cpp
+++ string/Remove All Occurrences of a Substring.cpp
@@ -1,10 +1,27 @@
-class Solution {
+class Solution
+{
 public:
-    string removeOccurrences(string s, string part) {
-        int pos=s.find(part);
-        while(pos!=string::npos){
-            s.erase(pos,part.length());
-            pos=s.find(part);
+    string removeOccurrences(string s, string part)
+    {
+        int pos = s.find(part);
+        while (pos != string::npos)
+        {
+            s.erase(pos, part.length());
+            pos = s.find(part);
+        }
+        return s;
+    }
+};
+
+// easy code
+class Solution
+{
+public:
+    string removeOccurrences(string s, string part)
+    {
+        while (s.length() > 0 && s.find(part) < s.length())
+        {
+            s.erase(s.find(part), part.length());
         }
         return s;
     }
