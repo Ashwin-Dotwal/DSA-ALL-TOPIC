56a611b6247eaca3a1322da2a3b148a05af11899
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-04 23:16:40 +0530

    add new code

--- Array/findmissingelement.cpp
+++ Array/findmissingelement.cpp
@@ -1,38 +1,29 @@
 
-//BRUT FORCE SOLUTION
-class Solution {
-public:
-int brute(vector<int>&nums)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int missingNumber(vector<int> &a, int N)
 {
-     int flg=0;
-      int n=nums.size();
-      for(int i=0;i<=n;i++)  
-      {
-          int flg=0;
-          for(int j=0;j<n;j++)
-          {
-              if(i==nums[j])
-              {
-                  flg=1;
-                  break;
-              }
-          }
-          if(flg==0) return i;
 
-      }
-      return 1;
-}
-}
+    int xor1 = 0, xor2 = 0;
 
-//OPTIMAL SOLUTION
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int x = nums.size();
-        for(int i=0;i<nums.size();i++){
-            x=x^i;
-            x=x^nums[i];
-        }
-        return x;
+    for (int i = 0; i < N - 1; i++)
+    {
+        xor2 = xor2 ^ a[i];    // XOR of array elements
+        xor1 = xor1 ^ (i + 1); // XOR up to [1...N-1]
     }
-};
+    xor1 = xor1 ^ N; // XOR up to [1...N]
+
+    return (xor1 ^ xor2); // the missing number
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
