e7e2ba17aeddb82e603bed1f9f6a18628028a9f5
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-05 22:53:10 +0530

    add new code

--- Array/majorityelementisnby2.cpp
+++ Array/majorityelementisnby2.cpp
@@ -1,21 +1,33 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        
-        
-        for(int i=0;i<nums.size();i++){
-            int count=0;
-            for(int j=0;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    count++;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-                }
-                if(count>nums.size()/2){
-                    return nums[i];
-                }
+int majorityElement(vector<int> &nums)
+{
 
-            } 
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int count = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                count++;
+            }
+            if (count > nums.size() / 2)
+            {
+                return nums[i];
+            }
         }
-       return -1;
     }
-};
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> nums = {1, 2, 1, 2, 1, 2, 5, 2, 2};
+    int ans = majorityElement(nums);
+    cout << "majority element is" << ans << endl;
+    return 0;
+}
