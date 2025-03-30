ee37d753c749c88bf1081159b26f2c637190203e
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-30 22:43:04 +0530

    add linked list code

---
+++ linkedlist/reverselinkedlist2.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+  Node *reverseList(struct Node *head)
+  {
+
+    // code here
+    Node *prev = NULL;
+    Node *curr = head;
+    while (curr != NULL)
+    {
+      Node *temp = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = temp
+    };
+    return prev;
+  }
+};
