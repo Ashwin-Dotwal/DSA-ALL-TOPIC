5ee3e26e4b9b8caab99653e30a4dc4ac477b0480
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-06-07 17:20:30 +0530

    add new code

--- Array/findduplicate.cpp
+++ Array/findduplicate.cpp
@@ -1,26 +1,4 @@
-// slow fast pointer approch
-class Solution
-{
-public:
-    int findDuplicate(vector<int> &nums)
-    {
-        int slow = nums[0];
-        int fast = nums[0];
-        do
-        {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
 
-        fast = nums[0];
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
-    }
-};
 
 // first aproch
 class Solution
@@ -40,42 +18,10 @@ public:
     }
 };
 
+
+
+
 // second approch
-class Solution
-{
-public:
-    int findDuplicate(vector<int> &nums)
-    {
-        int ans = -1;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int index = abs(nums[i]);
-            if (nums[index] < 0)
-            {
-                ans = index;
-                break;
-            }
-            nums[index] *= -1;
-        }
-        return ans;
-    }
-};
-
-// third approch
-// positioning
-class Solution
-{
-public:
-    int findDuplicate(vector<int> &nums)
-    {
-        while (nums[0] != nums[nums[0]])
-        {
-            swap(nums[0], nums[nums[0]]);
-        }
-        return nums[0];
-    }
-};
-
 class Solution
 {
 public:
