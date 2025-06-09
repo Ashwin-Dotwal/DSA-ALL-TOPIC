c66954abff557add8c67567ad6682b4007899af5
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-06-09 17:35:39 +0530

    add some code in dsa

---
+++ BinarySearch/findLastOccurence.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+  vector<int> findFirstOccurance(vector<int> &nums, int target)
+  {
+    int s = 0;
+    int e = nums.size() - 1;
+    int ans = -1;
+    while (s <= e)
+    {
+      int mid = (s + e) / 2;
+      if (nums[mid] == target)
+      {
+        ans = mid;
+
+        s = mid + 1;
+      }
+      else if (target < nums[mid])
+      {
+        e = mid - 1;
+      }
+      else if (target > nums[mid])
+      {
+        s = mid + 1;
+      }
+      return ans;
+    }
+  }
+};
