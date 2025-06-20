b799e1cf4aeb50816a8bd53b5bf6c57bca540649
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-06-20 12:14:56 +0530

    add some code in recursion

---
+++ linkedlist/SwapPairs.cpp
@@ -0,0 +1,23 @@
+i // input: head = [1,2,3,4]
+
+    // Output: [2,1,4,3]
+
+    class Solution
+{
+public:
+  ListNode *swapPairs(ListNode *head)
+  {
+    if (head == NULL || head->next == NULL)
+    {
+      return head;
+    }
+
+    ListNode *temp = head->next;
+
+    head->next = swapPairs(temp->next);
+
+    temp->next = head;
+
+    return temp;
+  }
+};
