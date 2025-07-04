fcda48c3db5f5d3926637159769313fb77ea9cb4
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-07-04 23:29:03 +0530

    add new code in dp

--- DynamicProgramming/SpaceOptimization.cpp
+++ DynamicProgramming/SpaceOptimization.cpp
@@ -1,19 +1,24 @@
+
+
 class Solution
 {
 public:
   int climbStairs(int n)
   {
+
     if (n == 0 || n == 1)
     {
       return 1;
     }
-    int prev = 1, curr = 1;
+
+    int prev2 = 1, prev1 = 1; // initally at 0th and 1st index
+
     for (int i = 2; i <= n; i++)
     {
-      int temp = curr;
-      curr = prev + curr;
-      prev = temp;
+      int curr = prev1 + prev2;
+      prev2 = prev1; // update pointers
+      prev1 = curr;
     }
-    return curr;
+    return prev1;
   }
 };
