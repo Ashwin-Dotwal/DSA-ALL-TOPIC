7fa259f50a60262c553365fdbe1b64aed1f6c1f8
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-08-18 21:38:50 +0530

    add new code

--- linkedlist/Add2numberbylinkedlist.cpp
+++ linkedlist/Add2numberbylinkedlist.cpp
@@ -1,60 +1,99 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node*next;
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-    }
-}
-void print(Node*head){
-    Node*temp=head;
-    while (temp!=NULL)
+    Node *next;
+    Node(int data)
     {
-        cout<<temp->data<<" ";
-        temp=temp->next;
-        
+        this->data = data;
+        this->next = NULL;
     }
-    cout<<endl;
+} void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
-Node*reverse(Node*head){
-    Node*prev=NULL;
-    Node*curr=head;
-    Node*next=curr->next;
-    while (curr!=NULL)
+Node *reverse(Node *head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = curr->next;
+    while (curr != NULL)
     {
 
-    next=Curr->next;
-    curr->next=prev;
-    prev=curr;
-    curr=next;
-        
+        next = Curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
     return prev;
-    
 }
 
+// reverse ans linked list
 
+int main()
+{
+    Node *head1 = new Node(2);
+    Node *second2 = new Node(4);
+    head1->next1 = second1;
 
+    Node *head2 = new Node(2);
+    Node *second2 = new Node(3);
+    Node *third2 = new Node(4);
+    head2->next = second2;
+    second2->next = third2;
 
-    //reverse ans linked list
+    Node *ans = solve(head1, head2);
 
-
-int main(){
-    Node*head1=new Node(2);
-    Node*second2=new Node(4);
-    head1->next1=second1;
-
-    Node*head2=new Node(2);
-    Node*second2=new Node(3);
-    Node*third2=new Node(4);
-    head2->next=second2;
-    second2->next=third2;
-
-   Node*ans= solve(head1,head2);
-
-   print (ans);
+    print(ans);
     return 0;
 }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode dummy(0); // dummy head
+        ListNode *curr = &dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry)
+        {
+            int sum = carry;
+            if (l1)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummy.next;
+    }
+};
