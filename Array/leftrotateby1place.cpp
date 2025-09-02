faed8878c02aa0544145d99dd7c1a1a2c231a699
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-09-02 12:34:00 +0530

    add code

--- Array/leftrotateby1place.cpp
+++ Array/leftrotateby1place.cpp
@@ -1,14 +1,32 @@
 
 
-//left rotate by 1 place;
+// left rotate by 1 place;
 
-#include <bits/stdc++.h> 
-vector<int> rotateArray(vector<int>& arr, int n) {
+#include <bits/stdc++.h>
+vector<int> rotateArray(vector<int> &arr, int n)
+{
     // Write your code here.
-    int temp=arr[0];
-    for(int i=1;i<n;i++){
-        arr[i-1]=arr[i];
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
     }
-    arr[n-1]=temp;
+    arr[n - 1] = temp;
     return arr;
 }
+
+// gfg
+class Solution
+{
+public:
+    void rotate(vector<int> &arr)
+    {
+        int n = arr.size();
+        int temp = arr[n - 1]; // store last element
+        for (int i = n - 1; i > 0; i--)
+        {
+            arr[i] = arr[i - 1]; // shift elements right
+        }
+        arr[0] = temp; // put last element at front
+    }
+};
